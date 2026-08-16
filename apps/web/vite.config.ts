33e14988e37ed73609796388ceef7c37ab2f0cb8
junbozhou88 <junbozhou88usa@gmail.com>
2026-08-16 14:54:33 +0800

    fix: 修改前端部署cloudfalre pages打不开页面问题

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,18 +4,19 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    port: 3001,
-  },
-  resolve: {
-    tsconfigPaths: true,
-  },
-  plugins: [
-    tailwindcss(),
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
-    react(),
-  ],
+	base: "/",
+	plugins: [
+		tailwindcss(),
+		tanstackRouter({
+			autoCodeSplitting: true,
+			target: "react",
+		}),
+		react(),
+	],
+	resolve: {
+		tsconfigPaths: true,
+	},
+	server: {
+		port: 3001,
+	},
 });
